b5d27e21bb42161ad43b04ea60196f10e92f466e
Lynne Rang <lynne.rang@gmail.com>
2019-04-14 20:59:32 -0600

    Fix example image popup alignment to window.

--- src/Card.js
+++ src/Card.js
@@ -6,43 +6,47 @@ class Card extends Component {
     super(props);
 
     this.state = {
-      
+      showExample: false
     }
   }
 
   showExample = () => {
-    document.querySelector('.popup').classList.remove('hidden');
+    this.setState({showExample: true});
   }
 
   closeExample = () => {
-    document.querySelector('.popup').classList.add('hidden');
+    this.setState({showExample: false});
   }
 
   render() {
-    let term = this.props.hasTerm ? this.props.cardData.term
-    : 'What am I?';
- 
     // const exampleId = this.props.cardData.id;
     const url = require(`./images/example28.png`);
+
+    let popup = this.state.showExample && <div className='popup'>
+        <img className='example-img' src={url} alt='visual example' onBlur={this.closeExample}/>
+        <i className='fas fa-times popup-close-btn' onClick={this.closeExample}> close</i>
+      </div>;
+ 
+    let term = this.props.hasTerm ? this.props.cardData.term
+    : 'What am I?';
+    
     const deleteBtn = this.props.canDelete && <i className='fa fa-trash' onClick={this.props.deleteMyCard}></i>
 
     return (
-      <article className='card'>
-        {deleteBtn}
-        <div className='popup hidden'>
-          <img className='example-img' src={url} alt='visual example' onBlur={this.closeExample}/>
-          <i className='fas fa-times popup-close-btn' onClick={this.closeExample}> close</i>
-        </div>
-        
-        <div className='card-top'>
-          <h4 className='term-header'>{term}</h4>
-        </div>
-        <div className='card-bottom'>
-          <h4 className='description-header'>Description: </h4>
-          <p className='description'>{this.props.cardData.definition}</p>
-          <p className='example-link' onClick={this.showExample}>(View Example)</p>
-        </div>
-        </article>
+      <>
+      {popup}
+        <article className='card'>
+          {deleteBtn}        
+          <div className='card-top'>
+            <h4 className='term-header'>{term}</h4>
+          </div>
+          <div className='card-bottom'>
+            <h4 className='description-header'>Description: </h4>
+            <p className='description'>{this.props.cardData.definition}</p>
+            <p className='example-link' onClick={this.showExample}>(View Example)</p>
+          </div>
+          </article>
+        </>
     )
   }
 }
