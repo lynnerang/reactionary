e79f04143db3b3f3fb1f4ab93303d9ebe05f0526
Lynne Rang <lynne.rang@gmail.com>
2019-04-17 01:07:26 -0600

    Updates

---
+++ precache-manifest.221005c18e08197b9512be8df6c5b8d5.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed1bdd9f8c1c1bc7ce11",
+    "url": "/reactionary/static/css/main.843a7967.chunk.css"
+  },
+  {
+    "revision": "ed1bdd9f8c1c1bc7ce11",
+    "url": "/reactionary/static/js/main.c0d6e9bb.chunk.js"
+  },
+  {
+    "revision": "4d8b878a3db6e581b2df",
+    "url": "/reactionary/static/js/runtime~main.ad9736d5.js"
+  },
+  {
+    "revision": "474fb522fd606a02b695",
+    "url": "/reactionary/static/js/2.bdf977ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactionary/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "80873aacc4cd572699ea8772005e4138",
+    "url": "/reactionary/static/media/head2.80873aac.png"
+  },
+  {
+    "revision": "2738fa46c1a21e98f2dbe333b1816569",
+    "url": "/reactionary/static/media/example1.2738fa46.png"
+  },
+  {
+    "revision": "880701d0f840aa6bc472a99d296c5cf8",
+    "url": "/reactionary/static/media/example10.880701d0.png"
+  },
+  {
+    "revision": "8e0bb9ba59b9e5740ec7d0cbe2ab79b0",
+    "url": "/reactionary/static/media/example11.8e0bb9ba.png"
+  },
+  {
+    "revision": "3d587e0055b73cea040141e713e11498",
+    "url": "/reactionary/static/media/example13.3d587e00.png"
+  },
+  {
+    "revision": "d5c1103734c57c574bad7437a16a2b43",
+    "url": "/reactionary/static/media/example14.d5c11037.png"
+  },
+  {
+    "revision": "672e715ce82937425947a4117e21d3e2",
+    "url": "/reactionary/static/media/example17.672e715c.png"
+  },
+  {
+    "revision": "05d55db91cdfe50dc315164c1bbbd358",
+    "url": "/reactionary/static/media/example18.05d55db9.png"
+  },
+  {
+    "revision": "95dd48c5798bc29ae46f134812d889bc",
+    "url": "/reactionary/static/media/example19.95dd48c5.png"
+  },
+  {
+    "revision": "8d922dd218994e4ea9d1c29532ff71e7",
+    "url": "/reactionary/static/media/example20.8d922dd2.png"
+  },
+  {
+    "revision": "74fb23f3dcf456d99a7d54dc6ed97ed0",
+    "url": "/reactionary/static/media/example21.74fb23f3.png"
+  },
+  {
+    "revision": "cfdbf50166b65de1e5b7e452ddc013c8",
+    "url": "/reactionary/static/media/example22.cfdbf501.png"
+  },
+  {
+    "revision": "4f8704eeb9e164dd8d92ef0ae88572ff",
+    "url": "/reactionary/static/media/example23.4f8704ee.png"
+  },
+  {
+    "revision": "f0459c44bb8e546b8306d96aefbd188c",
+    "url": "/reactionary/static/media/example24.f0459c44.png"
+  },
+  {
+    "revision": "9b063faa29e6c5fa4cb17e6f246d3e0c",
+    "url": "/reactionary/static/media/example33.9b063faa.png"
+  },
+  {
+    "revision": "14b327c80957acd5801eb929add57144",
+    "url": "/reactionary/static/media/example34.14b327c8.png"
+  },
+  {
+    "revision": "efd11c57c067d543e11e7577cb9bbb62",
+    "url": "/reactionary/static/media/example35.efd11c57.png"
+  },
+  {
+    "revision": "c7f7390f3db241d5edcc35cc8edc8ed2",
+    "url": "/reactionary/static/media/example6.c7f7390f.png"
+  },
+  {
+    "revision": "270db16bf5ebd6f7282d8de02c9ec4e9",
+    "url": "/reactionary/static/media/example7.270db16b.png"
+  },
+  {
+    "revision": "d5af926c863613afd19e15e6d688abe1",
+    "url": "/reactionary/static/media/example8.d5af926c.png"
+  },
+  {
+    "revision": "c79dfd10bf3e4d227cb4c640d98da858",
+    "url": "/reactionary/static/media/example9.c79dfd10.png"
+  },
+  {
+    "revision": "de049da3971d417699917c093fcdcf9e",
+    "url": "/reactionary/index.html"
+  }
+];
